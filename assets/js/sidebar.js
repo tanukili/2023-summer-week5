f7331410cfe37756c08a6412ffb0c5b5c700cd18
tanukile <backpacker320@gmail.com>
2023-08-22 03:40:54 +0800

    sideber - jQuery - fixed

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".sidebar .nav-link").click(function(e) {
-      e.preventDefault();
+      // e.preventDefault(); 有加的話，連結會被取消動作
       $(this).parent().siblings().find(".nav-link").removeClass("active"); // 回到父層 > 同層其他項目 > 找到目標
       $(this).addClass("active");
   });
